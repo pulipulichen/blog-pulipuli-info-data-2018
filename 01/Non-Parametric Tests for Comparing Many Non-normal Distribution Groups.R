180c595f8f6c43ff4cd89aa9b3201f1206a56557
Pulipuli Chen <pulipuli.chen@gmail.com>
2018-01-28 11:18:47 +0800

    0128 加入常態性檢定

--- 01/Non-Parametric Tests for Comparing Many Non-normal Distribution Groups.R
+++ 01/Non-Parametric Tests for Comparing Many Non-normal Distribution Groups.R
@@ -1,3 +1,9 @@
+#csv_file_path = file.choose()
+csv_file_path = 'D:\\Desktop\\20180127 中位數比較SPSS\\Kruskal–Wallis test example - data.csv'
+
+timestamp = format(Sys.time(), "%m%d-%H%M")
+timestamp = '0000'
+
 if(!require(dplyr)){install.packages("dplyr")}
 if(!require(FSA)){install.packages("FSA")}
 if(!require(lattice)){install.packages("lattice")}
@@ -7,18 +13,13 @@ if(!require(rcompanion)){install.packages("rcompanion")}
 if(!require(multcompView)){install.packages("multcompView")}
 if(!require(userfriendlyscience)){install.packages("userfriendlyscience")}
 
-csv_file_path = file.choose()
 csv_dir_path = dirname(csv_file_path)
 csv_file_name = sub(pattern = "(.*)\\..*$", replacement = "\\1", basename(csv_file_path))
-
-timestamp = format(Sys.time(), "%m%d-%H%M")
-
-output_file_path = file.path(csv_dir_path, paste(csv_file_name, '_', timestamp, '_kw-test_output.txt', sep=""))
-
+output_file_path = file.path(csv_dir_path, paste(csv_file_name, '_', timestamp, '_result.txt', sep=""))
 
 Data = read.csv(csv_file_path, header=TRUE, sep=",")
 
-cat("### File: ", csv_file_path, "\n", file=output_file_path, sep="\n", append=FALSE)
+cat("### File\n", csv_file_path, file=output_file_path, sep="\n", append=FALSE)
 
 ### Specify the order of factor levels
 
@@ -30,7 +31,7 @@ Data = mutate(Data, group = factor(group, levels=unique(group)))
 library(FSA)
 
 out <- capture.output(Summarize(value ~ group, data = Data))
-cat("### Medians and descriptive statistics\n", out, file=output_file_path, sep="\n", append=TRUE)
+cat("\n### Medians and descriptive statistics\n", out, file=output_file_path, sep="\n", append=TRUE)
 
 ### Graphing the results ###
   
@@ -38,32 +39,45 @@ cat("### Medians and descriptive statistics\n", out, file=output_file_path, sep=
 
 library(lattice)
 
-
 png(file.path(csv_dir_path, paste(csv_file_name, '_', timestamp, '_histogram.png', sep="")))
+histogram(~ value | group,  data=Data, layout=c(1,length(levels(Data$group))), 
+   panel=function(x, ...) {                             
+   panel.histogram(x, ...) 
 
-histogram(~ value | group,  data=Data, layout=c(1,length(levels(Data$group))))
-
+   panel.mathdensity(dmath = dnorm, col = "black",
+                                args = list(mean=mean(x),sd=sd(x)))          
+                                        })
 dev.off()
 
 # Simple boxplots of values across groups
 
 png(file.path(csv_dir_path, paste(csv_file_name, '_', timestamp, '_boxplot.png', sep="")))
-
 boxplot(value ~ group,
         data = Data,
         ylab="Value",
         xlab="Group")
-
 dev.off()
+
+### Shapiro-Wilk normality test
+
+cat("\n### Shapiro-Wilk normality test\n", file=output_file_path, sep="\n", append=TRUE)
+
+Data.levels = split(Data, Data$group)
+for(i in seq(length(Data.levels))) {
+    group.name = Data.levels[[i]]$group[1]
+    cat(paste("Group: ", group.name, sep=''), file=output_file_path, sep="", append=TRUE)
+    shapiro.result = shapiro.test(Data.levels[[i]]$value)
+    cat(", W = ", shapiro.result$statistic, " p-value = ", shapiro.result$p.value, "\n" , file=output_file_path, sep="", append=TRUE)
+}
 
 ### Heteroscedasticity test
 
 library(car)
 leveneTest.result = leveneTest(value~group, Data)
 out <- capture.output(leveneTest.result)
-cat("\n### Heteroscedasticity test\n", out, file=output_file_path, sep="\n", append=TRUE)
+cat("\n### Test for Homogeneity of Variance\n", out, file=output_file_path, sep="\n", append=TRUE)
 
-is.heteroscedastic = (leveneTest.result$`Pr(>F)` < 0.05)
+is.heteroscedastic = (leveneTest.result$`Pr(>F)` <= 0.05)
 
 if (is.heteroscedastic) {
     cat("\nData are heteroscedastic. Excute Welch's anova.", file=output_file_path, sep="\n", append=TRUE)
@@ -75,7 +89,7 @@ if (is.heteroscedastic) {
 
 if (is.heteroscedastic) {
 
-### Welch’s anova for unequal variances
+### Welch's anova for unequal variances
 
 out <- capture.output(oneway.test(value ~ group,
             data=Data,
@@ -92,8 +106,11 @@ cat("\n### Games-Howell Post-Hoc Test\n", out, file=output_file_path, sep="\n", 
 
 ### Kruskal–Wallis test
 
-out <- capture.output(kruskal.test(value ~ group, data = Data))
+kruskal.result = kruskal.test(value ~ group, data = Data)
+out <- capture.output(kruskal.result)
 cat("\n### Kruskal–Wallis test for equal variances", out, file=output_file_path, sep="\n", append=TRUE)
+ 
+cat(paste("Eta squared: ", kruskal.result$statistic / (length(Data$group) - 1), "\n", sep=''), file=output_file_path, sep="\n", append=TRUE)
 
 ### Dunn test for multiple comparisons
 
@@ -112,3 +129,5 @@ PT = dunnTest(value ~ group,
 out <- capture.output(PT)
 cat("### Dunn test\n", out, file=output_file_path, sep="\n", append=TRUE)
 }
+
+message('Finish')
